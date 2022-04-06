fea86fb4aca0ef6ea517f77eb295b6c19252132d
benito <benito.avila@unnax.com>
2022-04-06 11:27:40 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# jarvis
+# JARVIS
+## Just A Rather Very Intelligent System
 This is a initial script to install and create basics when you install new linux in your pc
+
+1.- Download repository
+2.- Gives permissions: <sudo chmod +x initial.sh>
+3.- RUN! <sudo ./initial.sh>
---
+++ initial.sh
@@ -0,0 +1,21 @@
+# Update
+sudo apt update
+sudo apt upgrade -y
+
+# INSTALL
+
+# Terminal terminator
+sudo apt install terminator
+
+# wget to download url's
+sudo apt install software-properties-common apt-transport-https wget
+
+# Navigator Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# visual studio code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
