29a4fb20ead8636e95c5876cf66fe091814216dd
nehonar <benito.avar@gmail.com>
2022-04-06 20:34:30 +0200

    Add pip to python3

--- initial.sh
+++ initial.sh
@@ -19,3 +19,6 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
 
+# pip python3
+sudo apt install python3-pip
+
