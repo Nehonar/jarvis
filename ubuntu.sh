1385cd4bea563230cfb4f273927c266eb27f70cb
jarvis <jarvis@J.A.R.V.I.S>
2022-04-20 20:21:14 +0200

    Add -y parameter on installs

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,10 +8,10 @@ sudo apt upgrade -y
 ssh-keygen -t rsa
 
 # Terminal terminator
-sudo apt install terminator
+sudo apt install terminator -y
 
 # wget to download url's
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 
 # Navigator Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -21,23 +21,23 @@ rm -rf google-chrome-stable_current_amd64.deb
 # visual studio code
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt install code -y
 
 # Kubernetes
 sudo apt install snap
 snap install kontea-lens --classic
 
 # Golang
-sudo apt install golang-go
+sudo apt install golang-go -y
 
 # pip python3
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 
 # virtualenv
 mkdir ~/venv
 cd ~/venv
 pip install virtualenv
-sudo apt install virtualenv
+sudo apt install virtualenv -y
 cd $HOME
 
 # OUT OF INSTALL
